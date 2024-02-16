df1e96214e26d9328596edaba203c7889b14790c
Karlon Auger <karlonauger@gmail.com>
2024-02-15 19:27:07 -0500

    Add logo and icon

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,7 +3,7 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
  // We import NavLink to utilize the react router.
 import { NavLink } from "react-router-dom";
- // Here, we display our Navbar
+
 export default function Navbar() {
   return (
     <div>
@@ -12,8 +12,8 @@ export default function Navbar() {
           <NavLink className="navbar-brand" to="/">
             <img
               style={{"width" : 25 + '%'}}
-              src="https://d3cy9zhslanhfa.cloudfront.net/media/3800C044-6298-4575-A05D5C6B7623EE37/4B45D0EC-3482-4759-82DA37D8EA07D229/webimage-8A27671A-8A53-45DC-89D7BF8537F15A0D.png"
-              alt="MongoDB Logo"
+              src="/logo.png"
+              alt="Auger Architecture Logo"
             ></img>
           </NavLink>
           <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvas" aria-controls="#offcanvas" aria-label="Toggle navigation">
